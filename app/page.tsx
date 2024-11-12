1234a72db171b25ce7c889826165514ba8c50b0c
kiara <kiaritapino11@gmail.com>
2024-11-12 08:52:12 -0400

    Inicio2

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Header from "@/components/Header";
+import Food from "@/components/Food";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Food/>
     </div>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -14,9 +14,18 @@ const Header = () => {
           height={500}
           alt={'Logo Hotel Pairumani'}
         />
-        <h1>Hotel Pairumani</h1>
+        <h1 className="titulo">Hotel Pairumani</h1>
       <Barpro/>
       </div>
+      <Image
+          className="inicio"
+          src="/inicio.png"
+          width={375}
+          height={300}
+          alt={'Imagen Inicio'}
+        />
+        <h2 className="bien">Bienvenido al Hotel</h2>
+        <h2 className="bien">Pairumani</h2>
     </header>
   );
 };
