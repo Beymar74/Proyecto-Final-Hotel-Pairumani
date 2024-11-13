29c053b4a90862c4f569bebefa3e83c9ea712a96
beymar <cruzbeymar74@gmail.com>
2024-11-13 18:18:17 -0400

    contac_admin

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
-import Image from "next/image";
-import Header from "@/components/Header";
-import Food from "@/components/Food";
+import React from 'react';
+import Header from '@/components/Header';
+import Food from '@/components/Food';
+import ContactSection from '@/components/organisms/ContactSection';
+import Footer from '@/components/organisms/Footer';
 
 export default function Home() {
-  return (
-    <div>
-      <Header />
-      <Food />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Food />
+            <ContactSection />
+            <Footer />
+        </div>
+    );
 }
