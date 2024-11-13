73a160dc7bddd3c0919e1d31223fcc0a3e31eab9
kiara <kiaritapino11@gmail.com>
2024-11-13 15:13:34 -0400

    pagina3/crearcuentav1

---
+++ app/crear/componentsCre/Create.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Create.css";
+
+const Create = () => {
+  return (
+    <div className="form">
+      <form className="registro">
+        <h1 className="info">Nombre Completo</h1>
+        <input type="text"  placeholder="Nombre Completo" />
+
+        <h1 className="info">Email</h1>
+        <input type="email"  placeholder="ejemplo@dominio.com" />
+
+        <h1 className="info">Contraseña</h1>
+        <input type="password"  placeholder="Contraseña" />
+
+        <h1 className="info">Confirmar Contraseña</h1>
+        <input
+          type="password"
+          
+          placeholder="Confirmar Contraseña"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Create;
---
+++ app/crear/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Create from "./componentsCre/Create"
+import Buttons from "./componentsCre/Button"
+
+const CrearPage = () => {
+  return (
+    <div>
+        <Create/>
+        <Buttons/>
+    </div>
+  )
+}
+
+export default CrearPage
