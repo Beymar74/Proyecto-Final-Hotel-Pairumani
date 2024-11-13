573cafb9c873342d2c2e79292bb75ea14c76438c
kiara <kiaritapino11@gmail.com>
2024-11-13 16:09:27 -0400

    pagina2-3v5

--- app/menuini/components4/Header3.tsx
+++ app/menuini/components4/Header3.tsx
@@ -7,22 +7,22 @@ const Header3 = () => {
   return (
     <header>
       <div className="cont2">
-      <Image
-      className="pairumani1"
-        src="/logoP.png"
-        width={100}
-        height={100}
-        alt={'Logo Hotel Pairumani'}
-      />
-      <h1 className="h1-2">Hotel Pairumani</h1>
-      <Navbar/>
-      <Image
-      className="profile1"
-        src="/icono.png"
-        width={100}
-        height={100}
-        alt={'Icono del perfil'}
-      />
+        <Image
+          className="pairumani1"
+          src="/logoP.png"
+          width={100}
+          height={100}
+          alt={"Logo Hotel Pairumani"}
+        />
+        <h1 className="h1-2">Hotel Pairumani</h1>
+        <Navbar />
+        <Image
+          className="profile1"
+          src="/icono.png"
+          width={100}
+          height={100}
+          alt={"Icono del perfil"}
+        />
       </div>
     </header>
   );
