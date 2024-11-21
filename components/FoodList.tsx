96fd30e0669f91946cb8765f8622ec963372c53f
beymar <cruzbeymar74@gmail.com>
2024-11-21 10:38:54 -0400

    Correccion platos

--- components/FoodList.tsx
+++ components/FoodList.tsx
@@ -4,9 +4,9 @@ import FoodItem from './FoodItem';
 
 const foodData = [
     { id: '1', src: '/images/image1.jpg', alt: 'Desayuno Americano', name: 'Desayuno Americano', stars: 5 },
-    { id: '2', src: '/images/image2.jpg', alt: 'Ensalada de langosta', name: 'Ensalada de langosta', stars: 5 },
-    { id: '3', src: '/images/image3.jpg', alt: 'Pique Macho', name: 'Pique Macho', stars: 5 },
-    { id: '4', src: '/images/image4.jpg', alt: 'Pampaku', name: 'Pampaku', stars: 5 },
+    { id: '16', src: '/images/image2.jpg', alt: 'Ensalada de langosta', name: 'Ensalada de langosta', stars: 5 },
+    { id: '6', src: '/images/image3.jpg', alt: 'Pique Macho', name: 'Pique Macho', stars: 5 },
+    { id: '7', src: '/images/image4.jpg', alt: 'Pampaku', name: 'Pampaku', stars: 5 },
 ];
 
 const FoodList = () => {
