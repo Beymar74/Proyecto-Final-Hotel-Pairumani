01400266b35d3870f70958b3b6aee4fe2ab63d25
Mauri <maumenachotri224@gmail.com>
2024-11-26 00:23:10 -0300

    PantallaConfirmacion

---
+++ app/confirmar/page.tsx
@@ -0,0 +1,13 @@
+import Header3 from "@/components/Header3";
+import React from "react";
+import ContenidoConfirmar from "@/components/ContenidoConfirmar";
+const page = () => {
+  return (
+    <div>
+      <Header3 />
+      <ContenidoConfirmar />
+    </div>
+  );
+};
+
+export default page;
